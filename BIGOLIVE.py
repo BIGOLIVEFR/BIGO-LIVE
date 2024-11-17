cf6f3dead9e02bb5b98c648810855a67d4a005d9
BIGOLIVEFR <bigoloivefrench@gmail.com>
2024-11-17 11:43:14 +0100

    Update BIGOLIVE.py

--- BIGOLIVE.py
+++ BIGOLIVE.py
@@ -13,3 +13,13 @@ element.click()
 
 # Close the browser
 driver.quit()
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# Wait for the element to be present and clickable
+element = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.XPATH, "//*[contains(@class, 'host-follow-btn')]"))
+)
+element.click()
